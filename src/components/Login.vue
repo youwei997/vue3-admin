d21a0118144db58d23320192d19638582a05e6cd
youwei <1690312648@qq.com>
2021-12-21 22:05:07 +0800

    vue3 admin 更新全局axios请求方法

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,13 +4,22 @@
 </template>
 <script setup>
 import {useRouter} from "vue-router";
+import {getCurrentInstance, onMounted} from "vue";
 
 const router = useRouter()
-const goHome = ()=>{
+const goHome = () => {
 	router.push({
-		path:'/welcome'
+		path: '/welcome'
 	})
 }
+
+const {proxy} = getCurrentInstance()
+
+onMounted(() => {
+	proxy.$request.get('http://127.0.0.1:4523/mock/482937/pet/1',{name:'zs'},{loading:true}).then((res)=>{
+		console.log(res)
+	})
+})
 </script>
 <style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from './utils/request'
+import request from './utils/request'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 //全局配置
-app.config.globalProperties.$http=axios;
+app.config.globalProperties.$request = request;
 app.use(ElementPlus).use(router).mount('#app')
