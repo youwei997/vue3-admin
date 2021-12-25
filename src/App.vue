f3366dc4577e2398ecc9da9f8ddd9649c53c92a6
youwei <1690312648@qq.com>
2021-12-25 13:58:51 +0800

    app.vue文件，去除无用代码

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,6 @@
 	<router-view></router-view>
 </template>
 <script setup>
-import {getCurrentInstance, onMounted} from "vue";
-
-const {proxy} = getCurrentInstance()
-
-onMounted(()=>{
-	proxy.$storage.setItem('userInfo',{name:'zs',age:'15'})
-	console.log(proxy.$storage.getItem('userInfo'))
-	proxy.$storage.clearAll()
-})
 </script>
 
 <style>
