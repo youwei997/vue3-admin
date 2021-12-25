bcd515f303a3d24413f3586ceb2aae12aec7d62f
youwei <1690312648@qq.com>
2021-12-25 14:34:05 +0800

    登录页 登录请求跳转完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="login-wrapper">
 		<div class="model">
-			<el-form>
+			<el-form :model="user" status-icon :rules="rules" ref="loginFormRef">
 				<div class="title">火星</div>
-				<el-form-item>
-					<el-input type="text" :prefix-icon="elIcons.User"/>
+				<el-form-item prop="userName">
+					<el-input type="text" :prefix-icon="elIcons.User" v-model="user.userName"/>
+				</el-form-item>
+				<el-form-item prop="userPwd">
+					<el-input type="password" :prefix-icon="elIcons.Lock" v-model="user.userPwd"/>
 				</el-form-item>
 				<el-form-item>
-					<el-input type="password"  :prefix-icon="elIcons.Lock" />
-				</el-form-item>
-				<el-form-item>
-					<el-button class="btn-login" type="primary">登录</el-button>
+					<el-button class="btn-login" type="primary" @click="login">登录</el-button>
 				</el-form-item>
 			</el-form>
 		</div>
@@ -18,23 +18,60 @@
 </template>
 <script setup>
 import {useRouter} from "vue-router";
-import {getCurrentInstance, onMounted} from "vue";
+import {getCurrentInstance, onMounted, reactive, ref} from "vue";
+import {useStore} from "vuex"
 
+//获取路由实例
 const router = useRouter()
+const store = useStore()
+
+//全局属性
+const {proxy} = getCurrentInstance()
+
+//跳转home页
 const goHome = () => {
 	router.push({
 		path: '/welcome'
 	})
 }
 
-const {proxy} = getCurrentInstance()
-
-onMounted(() => {
-	console.log(proxy.elIcons)
-	// proxy.$request.get('https://api.thecatapi.com/v1/images/search?limit=1',{name:'zs'},{loading:true}).then((res)=>{
-	// 	console.log(res)
-	// })
+//定义用户信息
+const user = reactive({
+	userName: '',
+	userPwd: ''
 })
+
+
+//定义登录表单校验
+const rules = reactive({
+	userName: [
+		{
+			required: true,
+			message: '请输入用户名',
+			trigger: "blur",
+		}
+	],
+	userPwd: [
+		{
+			required: true,
+			message: '请输入密码',
+			trigger: "blur",
+		}
+	]
+})
+
+//获取表单的ref
+const loginFormRef = ref()
+
+//登录方法
+const login = () => {
+	loginFormRef.value.validate(valid => {
+		proxy.$api.login(user).then(res => {
+			store.commit("saveUserInfo", res)
+			router.push('/welcome')
+		})
+	})
+}
 </script>
 <style scoped lang="scss">
 .login-wrapper {
@@ -44,19 +81,22 @@ onMounted(() => {
 	background-color: #f9fcff;
 	width: 100vw;
 	height: 100vh;
-	.model{
+
+	.model {
 		width: 500px;
 		padding: 20px;
 		background-color: #fff;
 		border-radius: 5px;
 		box-shadow: 0px 0px 5px 3px #ccc;
-		.title{
+
+		.title {
 			font-size: 50px;
 			line-height: 1.5;
 			text-align: center;
 			margin-bottom: 30px;
 		}
-		.btn-login{
+
+		.btn-login {
 			width: 100%;
 		}
 	}
