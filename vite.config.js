043e5889e3db10ab992ac223e2f152b120bbb271
youwei <1690312648@qq.com>
2021-12-26 19:11:40 +0800

    安装eslint和prettier，但没启用，太麻烦

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     //     { find: '@', replacement: resolve(__dirname, 'src') }
     // ],
     alias:{
-      "@/": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     }
   },
    server: {
