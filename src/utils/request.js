ce4165676605b7f401a1334aaa341e60c5a6f25a
youwei <1690312648@qq.com>
2021-12-26 21:54:49 +0800

    header增加token

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import {ElMessage} from "element-plus";
 import config from "../config";
 import router from "../router";
+import storage from './storage'
 
 const TOKEN_INVALID = "token认证失败，请重新登陆";
 const NETWORK_ERROR = "网络请求异常，请稍后再试";
@@ -16,7 +17,8 @@ const service = axios.create({
 //请求拦截
 service.interceptors.request.use((req) => {
     const headers = req.headers;
-    if (!headers.Authorization) headers.Authorization = "you";
+    const {token} = storage.getItem('userInfo')
+    if (!headers.Authorization) headers.Authorization = 'Bearer ' + token;
     return req
 });
 
