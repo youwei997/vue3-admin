7edc25211e7d2d7010d794b5c7da68412d810c46
youwei <1690312648@qq.com>
2021-12-27 21:45:24 +0800

    config文件，mock改为线上mock地址

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,13 @@ const env = import.meta.env.MODE || "prod";
 const EnvConfig = {
     dev: {
         baseApi: "/api",
-        mockApi:'http://127.0.0.1:4523/mock/526154'+ '/api',
+        // mockApi:'http://127.0.0.1:4523/mock/526154'+ '/api',
+        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api',
     },
     test: {
         baseApi: "/api",
-        mockApi: 'http://127.0.0.1:4523/mock/526154'+ '/api',
+        // mockApi: 'http://127.0.0.1:4523/mock/526154'+ '/api',
+        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api',
     },
     prod: {
         baseApi: "/api",
