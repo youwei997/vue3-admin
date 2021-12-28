6e916bb22e6ce8ccfe7946dd4cb2631558740096
youwei <1690312648@qq.com>
2021-12-28 21:26:55 +0800

    更新注释

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as elIcons from '@element-plus/icons-vue'
-import request from './utils/request'
+// import request from './utils/request'
+// import axios from 'axios'
 import storage from './utils/storage'
 import store from './store'
 import api from './api'
 const app = createApp(App);
+
 //全局配置
-app.config.globalProperties.$request = request;
+// app.provide('axios',axios) //使用provide，子孙组件都可以使用inject获取到
+// app.config.globalProperties.$request = request; //使用getCurrentInstance可以取到
 app.config.globalProperties.$storage = storage;
 app.config.globalProperties.$api = api;
 app.config.globalProperties.elIcons = elIcons;
