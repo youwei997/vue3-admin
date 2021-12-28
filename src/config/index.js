ef949ae48dbbe96f9c9ed0769de152ba4a543c1e
youwei <1690312648@qq.com>
2021-12-28 21:29:12 +0800

    mock地址改为自己fastmock注册的mock

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,16 @@
 //环境变量封装
 const env = import.meta.env.MODE || "prod";
-//https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api/menu/list
+//https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api/menu/list //视频项目mock地址
 const EnvConfig = {
     dev: {
         baseApi: "/api",
         // mockApi:'http://127.0.0.1:4523/mock/526154'+ '/api',
-        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api',
+        mockApi:'https://www.fastmock.site/mock/61617b8af5c6888d381c41eff0f95c4e/api',
     },
     test: {
         baseApi: "/api",
         // mockApi: 'http://127.0.0.1:4523/mock/526154'+ '/api',
-        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api',
+        mockApi:'https://www.fastmock.site/mock/61617b8af5c6888d381c41eff0f95c4e/api',
     },
     prod: {
         baseApi: "/api",
@@ -19,7 +19,7 @@ const EnvConfig = {
 };
 export default {
     env: env,
-    mock: false, //全局mock
+    mock: true, //全局mock
     ...EnvConfig[env],
     namespace: "namespace",
 };
