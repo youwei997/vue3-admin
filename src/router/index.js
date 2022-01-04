2a9faed8805bc395913c587490359d2a48173b34
youwei <1690312648@qq.com>
2022-01-04 23:05:48 +0800

    新增角色管理页面。默认跳转login页

--- src/router/index.js
+++ src/router/index.js
@@ -4,52 +4,60 @@ import Home from '../components/Home.vue'
 const routes = [
     {
         name: 'home',
-        path: '/',
-        meta:{
-          title:'首页'
+        path: '/welcome',
+        meta: {
+            title: '首页'
         },
         component: Home,
-        redirect: '/welcome',
+        // redirect: '/welcome',
         children: [
             {
                 name: 'welcome',
                 path: '/welcome',
-                meta:{
-                    title:'欢迎页'
+                meta: {
+                    title: '欢迎页'
                 },
-                component: ()=>import('../views/Welcome.vue') ,
+                component: () => import('../views/Welcome.vue'),
             },
             {
                 name: 'user',
                 path: '/system/user',
-                meta:{
-                    title:'用户管理'
+                meta: {
+                    title: '用户管理'
                 },
-                component: ()=>import('../views/User.vue') ,
+                component: () => import('../views/User.vue'),
             },
             {
                 name: 'menu',
                 path: '/system/menu',
-                meta:{
-                    title:'菜单管理'
+                meta: {
+                    title: '菜单管理'
                 },
-                component: ()=>import('../views/Menu.vue') ,
+                component: () => import('../views/Menu.vue'),
+            },
+            {
+                name: 'role',
+                path: '/system/role',
+                meta: {
+                    title: '角色管理'
+                },
+                component: () => import('../views/Role.vue'),
             },
         ]
     },
 
     {
         name: 'login',
-        path: '/login',
-        meta:{
-            title:'登录页'
+        path: '/',
+        meta: {
+            title: '登录页'
         },
-        component: ()=>import('../views/Login.vue'),
+        component: () => import('../views/Login.vue'),
     },
 ]
 
 const router = createRouter({
-    history:createWebHashHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
