b2a4da9373a631e69653c468ec0ca4ac3a7b2c8a
youwei <1690312648@qq.com>
2022-01-09 20:32:35 +0800

    默认展开系统管理

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,17 @@
         <div :class="['nav-side', isCollapse ? 'fold' : 'unfold']">
             <!-- 系统LOGO -->
             <div class="logo">
-                <img src="./../assets/logo.png"/> <span>Manager</span>
+                <img src="./../assets/logo.png" alt=""/> <span>Manager</span>
             </div>
             <!-- 导航菜单 -->
             <el-menu
-                :default-active="activeMenu"
-                background-color="#001529"
-                text-color="#fff"
-                router
-                :collapse="isCollapse"
-                class="nav-menu"
+                    :default-openeds="['/system']"
+                    :default-active="activeMenu"
+                    background-color="#001529"
+                    text-color="#fff"
+                    router
+                    :collapse="isCollapse"
+                    class="nav-menu"
             >
                 <tree-menu :userMenu="userMenu"/>
             </el-menu>
@@ -32,9 +33,9 @@
                 </div>
                 <div class="user-info">
                     <el-badge
-                        :is-dot="noticeCount > 0 ? true : false"
-                        class="notice"
-                        type="danger"
+                            :is-dot="noticeCount > 0 ? true : false"
+                            class="notice"
+                            type="danger"
                     >
                         <el-icon>
                             <Bell></Bell>
@@ -144,99 +145,99 @@ onMounted(() => {
 </script>
 <style lang="scss">
 .basic-layout {
-    position: relative;
+  position: relative;
 
-    .nav-side {
-        position: fixed;
-        width: 200px;
-        height: 100vh;
-        background-color: #001529;
-        color: #fff;
-        overflow-y: auto;
-        transition: width 0.5s;
+  .nav-side {
+    position: fixed;
+    width: 200px;
+    height: 100vh;
+    background-color: #001529;
+    color: #fff;
+    overflow-y: auto;
+    transition: width 0.5s;
 
-        .logo {
-            display: flex;
-            align-items: center;
-            font-size: 18px;
-            height: 50px;
+    .logo {
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      height: 50px;
 
-            img {
-                margin: 0 16px;
-                width: 32px;
-                height: 32px;
-            }
-        }
-
-        .nav-menu {
-            height: calc(100vh - 50px);
-            border-right: none;
-        }
-
-        // 合并
-        &.fold {
-            width: 64px;
-        }
-
-        // 展开
-        &.unfold {
-            width: 200px;
-        }
+      img {
+        margin: 0 16px;
+        width: 32px;
+        height: 32px;
+      }
     }
 
-    .content-right {
-        margin-left: 200px;
-        // 合并
-        &.fold {
-            margin-left: 64px;
-        }
-
-        // 展开
-        &.unfold {
-            margin-left: 200px;
-        }
-
-        .nav-top {
-            height: 50px;
-            line-height: 50px;
-            display: flex;
-            justify-content: space-between;
-            border-bottom: 1px solid #ddd;
-            padding: 0 20px;
-
-            .nav-left {
-                display: flex;
-                align-items: center;
-
-                .menu-fold {
-                    margin-right: 15px;
-                    font-size: 18px;
-                }
-            }
-
-            .user-info {
-                .notice {
-                    line-height: 30px;
-                    margin-right: 15px;
-                }
-
-                .user-link {
-                    cursor: pointer;
-                    color: #409eff;
-                }
-            }
-        }
-
-        .wrapper {
-            background: #eef0f3;
-            padding: 20px;
-            height: calc(100vh - 50px);
-
-            .main-page {
-                background: #fff;
-                height: 100%;
-            }
-        }
+    .nav-menu {
+      height: calc(100vh - 50px);
+      border-right: none;
     }
+
+    // 合并
+    &.fold {
+      width: 64px;
+    }
+
+    // 展开
+    &.unfold {
+      width: 200px;
+    }
+  }
+
+  .content-right {
+    margin-left: 200px;
+    // 合并
+    &.fold {
+      margin-left: 64px;
+    }
+
+    // 展开
+    &.unfold {
+      margin-left: 200px;
+    }
+
+    .nav-top {
+      height: 50px;
+      line-height: 50px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #ddd;
+      padding: 0 20px;
+
+      .nav-left {
+        display: flex;
+        align-items: center;
+
+        .menu-fold {
+          margin-right: 15px;
+          font-size: 18px;
+        }
+      }
+
+      .user-info {
+        .notice {
+          line-height: 30px;
+          margin-right: 15px;
+        }
+
+        .user-link {
+          cursor: pointer;
+          color: #409eff;
+        }
+      }
+    }
+
+    .wrapper {
+      background: #eef0f3;
+      padding: 20px;
+      height: calc(100vh - 50px);
+
+      .main-page {
+        background: #fff;
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,27 +1,30 @@
 <template>
-	<template v-for="menu in userMenu">
-		<el-sub-menu
-			v-if="
+    <template v-for="menu in userMenu">
+        <el-sub-menu
+                v-if="
         menu.children &&
         menu.children.length > 0 &&
         menu.children[0].menuType == 1
       "
-			:key="menu._id"
-			:index="menu.path"
-		>
-			<template #title>
-				<el-icon><Menu /></el-icon>
-				<span>{{ menu.menuName }}</span>
-			</template>
-			<tree-menu :userMenu="menu.children" />
-		</el-sub-menu>
-		<el-menu-item
-			v-else-if="menu.menuType == 1"
-			:index="menu.path"
-			:key="menu._id"
-		>{{ menu.menuName }}</el-menu-item
-		>
-	</template>
+                :key="menu._id"
+                :index="menu.path"
+        >
+            <template #title>
+                <el-icon>
+                    <Menu/>
+                </el-icon>
+                <span>{{ menu.menuName }}</span>
+            </template>
+            <tree-menu :userMenu="menu.children"/>
+        </el-sub-menu>
+        <el-menu-item
+                v-else-if="menu.menuType == 1"
+                :index="menu.path"
+                :key="menu._id"
+        >{{ menu.menuName }}
+        </el-menu-item
+        >
+    </template>
 </template>
 <script setup>
 //vue3写法
@@ -29,13 +32,13 @@ import {Menu} from '@element-plus/icons-vue'//手动导入图标
 import {toRefs} from "vue";
 
 //defineProps不能直接解构，会失去响应式，值无法及时更新
-const props =  defineProps({
-	userMenu:{
-		type:Array,
-		default() {
-			return [];
-		},
-	},
+const props = defineProps({
+    userMenu: {
+        type: Array,
+        default() {
+            return [];
+        },
+    },
 })
 
 const {userMenu} = toRefs(props)
